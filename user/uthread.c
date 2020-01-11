ba747d314e5a49986281deac391de10a8a756969
MonKey Lee <zbaxrl@foxmail.com>
2020-01-11 15:00:45 +0800

    lab 06: syscall

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for kernel context switches.
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
+  struct context ctx;           /* saved context */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+
 void 
 thread_init(void)
 {
@@ -61,6 +82,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64) &t->ctx, (uint64) &next_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -74,7 +96,9 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+
+  t->ctx.ra = (uint64) func;
+  t->ctx.sp = (uint64) (&t->stack) + STACK_SIZE;
 }
 
 void 
