17564f543ea84a0a4e280abe310369d81aa1e3f7
MonKey Lee <zbaxrl@foxmail.com>
2020-02-07 13:23:40 +0800

    lab08: fs

---
+++ fs/kernel/stat.h
@@ -0,0 +1,12 @@
+#define T_DIR     1   // Directory
+#define T_FILE    2   // File
+#define T_DEVICE  3   // Device
+#define T_SYMLINK 4   // Symlink
+
+struct stat {
+  int dev;     // File system's disk device
+  uint ino;    // Inode number
+  short type;  // Type of file
+  short nlink; // Number of links to file
+  uint64 size; // Size of file in bytes
+};
